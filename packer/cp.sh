9776809e4a5aad78076dec6d32042c25536c8212
Moshe Blumberg <moshe@confluent.io>
2022-12-01 18:09:19 +0000

    commit

--- packer/cp.sh
+++ packer/cp.sh
@@ -5,6 +5,24 @@ version="7.3"
 
 echo "Using var version ${version}"
 
+
+
+## This is an option monitoring setup we will need to add config files as well and export into KAFKA_OPTS 
+# ### Version of Jolokia Agent Jar to Download
+# jolokia_version="1.6.2"
+# ### Full URL used for Jolokia Agent Jar Download. 
+# jolokia_jar_url="http://search.maven.org/remotecontent?filepath=org/jolokia/jolokia-jvm/${jolokia_version}/jolokia-jvm-${jolokia_version}-agent.jar"
+# wget jolokia_jar_url
+# chmod 0755 "jolokia-jvm-${jolokia_version}-agent.jar"
+
+# ### Version of JmxExporter Agent Jar to Donwload
+# jmxexporter_version="0.12.0"
+# ### Full URL used for Prometheus Exporter Jar Download. 
+# jmxexporter_jar_url="https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/${jmxexporter_version}/jmx_prometheus_javaagent-${jmxexporter_version}.jar"
+
+# wget jmxexporter_jar_url
+# chmod 0755 "jmx_prometheus_javaagent-${jmxexporter_version}.jar"
+
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 sudo apt-get upgrade -y
 sudo apt-get update -y
